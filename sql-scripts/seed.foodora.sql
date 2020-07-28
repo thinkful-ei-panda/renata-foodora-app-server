7aeeace0d35621bbdeb4b99144e4677d1660fa96
seraphyne <renatafd717@gmail.com>
2020-07-28 14:57:23 -0500

    lower case some users

--- sql-scripts/seed.foodora.sql
+++ sql-scripts/seed.foodora.sql
@@ -21,8 +21,8 @@ VALUES
 INSERT INTO foodora_restaurant (username, password, restaurant_name, restaurant_address, city, zip, state, phone, url, email)
 VALUES
 ('zumbo', 'thebestintown', 'Zumbo Sweets', '123 Whatever Ln', 'Dallas', '12122', null, null, 'www.zumbo.com', 'zumbo@gmail.com'),
-('hkGordon', 'hellkitchenftw', 'HK', '674 5th Ave', 'NY', '23533', 'NY', '888-654-8989', 'www.hk.com', 'hkgordon@gmail.com'),
-('Los Mocahetes', 'loschicos', 'Los Mocahetes', '3242 Moore Ln', 'Dallas', '23566', '987-563-2334', 'www.losmocha.com', 'losmocha@gmail.com'),
+('hkgordon', 'hellkitchenftw', 'HK', '674 5th Ave', 'NY', '23533', 'NY', '888-654-8989', 'www.hk.com', 'hkgordon@gmail.com'),
+('los mocahetes', 'loschicos', 'Los Mocahetes', '3242 Moore Ln', 'Dallas', '23566', '987-563-2334', 'www.losmocha.com', 'losmocha@gmail.com'),
 ('walsmart', 'weneedmorewalmart', 'Walsmart', '234234 Again Ln', 'NY', '23422', '324-233-2423', 'www.walsmart.com', 'walsmart@gmail.com'),
 ('picodegalo', 'salsaisawesome', 'Pico de Galo', '43 Nothing Ln', 'Indianapolis', '65687', null, null, 'www.picodegalo.com', 'picodegalo@gmail.com'),
 
